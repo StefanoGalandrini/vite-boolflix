4472c53b027d7fccf62dca6ef388a7dfaadfa1cb
StefanoGalandrini <stefe68@gmail.com>
2023-05-24 17:33:55 +0200

    Add AppHeader input emit and App.vue call to axios

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ export default {
 	data() {
 		return {
 			store,
+			inputSearch: "",
 		};
 	},
 
@@ -17,14 +18,32 @@ export default {
 		AppMainMovies,
 		AppMainSeries,
 	},
+
+	methods: {
+		searchMovieTitle(value) {
+			this.inputSearch = value;
+			const endpoint =
+				"https://api.themoviedb.org/3/search/movie?api_key=d3e8524b6cb601e8d53a4fb415a08a48";
+			const query = this.inputSearch.split(" ").join("+");
+			const language = "it-IT";
+			const moviesUrl = `${endpoint}&query=${query}&language=${language}`;
+			console.log(moviesUrl);
+			axios.get(moviesUrl).then((response) => {
+				this.store.movies = response;
+				console.log(this.store.movies);
+			});
+		},
+	},
 };
 </script>
 
 <template>
-	<AppHeader />
+	<AppHeader @title="searchMovieTitle" />{{ this.inputSearch }}
 
 	<AppMainMovies />
 	<AppMainSeries />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use "./assets/styles/general.scss" as *;
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,87 @@
 <script>
-export default {};
+import {store} from "../store";
+
+export default {
+	data() {
+		return {
+			store,
+			inputSearch: "",
+		};
+	},
+
+	methods: {
+		emitInputSearch(event) {
+			event.preventDefault();
+			this.$emit("title", this.inputSearch);
+		},
+	},
+};
 </script>
 
-<template></template>
+<template>
+	<header>
+		<div class="container">
+			<img src="../assets/img/logo.webp" alt="" />
 
-<style lang="scss" scoped></style>
+			<form action="" @submit="emitInputSearch">
+				<input
+					type="text"
+					v-model="inputSearch"
+					placeholder="Inserisci il titolo" />
+				<button>Search</button>
+			</form>
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+
+header {
+	height: 10vh;
+	background-color: black;
+
+	.container {
+		width: 70vw;
+		margin-inline: auto;
+		height: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	img {
+		max-width: 150px;
+	}
+}
+
+form {
+	display: flex;
+	align-items: center;
+
+	input {
+		height: 2.5rem;
+		width: 20rem;
+		font-size: 1.2rem;
+		padding: 0.5rem 0.5rem;
+		margin-right: 0.5rem;
+		padding: 0.5rem;
+	}
+
+	button {
+		font-size: 1.2rem;
+		line-height: 1rem;
+		font-weight: 600;
+		border: none;
+		background-color: $accentColor;
+		border-radius: 5px;
+		color: white;
+		padding: 0.75rem 1.5rem;
+		transition: all 250ms;
+
+		&:hover {
+			background-color: #a30810;
+		}
+	}
+}
+</style>
